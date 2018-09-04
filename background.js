5435af812fbf007a8392c2df6d5b43fbf8f213a1
Thomas Rory Gummerson <thomas@gummerson.no>
2018-09-04 19:58:08 +0200

    Massive fixes

--- background.js
+++ background.js
@@ -1,92 +1,40 @@
-let regexes = [];
+/* global browser matchPatternToRegExp */
 
-// from https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Match_patterns
-const matchPatternToRegExp = function(pattern) {
-    if (pattern === '') {
-        return (/^(?:http|https|file|ftp|app):\/\//);
-    }
+const mapPatterns = sites =>
+    Array.isArray(sites)
+        ? sites.map(matchPatternToRegExp)
+        : [];
 
-    const schemeSegment = '(\\*|http|https|ws|wss|file|ftp)';
-    const hostSegment = '(\\*|(?:\\*\\.)?(?:[^/*]+))?';
-    const pathSegment = '(.*)';
-    const matchPatternRegExp = new RegExp(
-        `^${schemeSegment}://${hostSegment}/${pathSegment}$`
-    );
+const filterHeaders = regexes =>
+    ({ documentUrl, type, responseHeaders }) =>
+        type === 'sub_frame' &&
+        documentUrl &&
+        regexes.find(regex => documentUrl.match(regex))
+            ? {
+                responseHeaders: responseHeaders
+                    .filter(({ name }) =>
+                        name.toLowerCase() !== 'x-frame-options'),
+            }
+            : {};
 
-    let match = matchPatternRegExp.exec(pattern);
-    if (!match) {
-         throw new TypeError(`"${pattern}" is not a valid MatchPattern`);
-    }
+const loadSites = sites =>
+    Promise.resolve(sites || browser.storage.local.get('sites'))
+        .then(({ sites }) => {
+            const filterer = filterHeaders(mapPatterns(sites));
 
-    let [, scheme, host, path] = match;
-    if (!host) {
-        throw new TypeError(`"${pattern}" does not have a valid host`);
-    }
+            browser.webRequest.onHeadersReceived.addListener(
+                filterer,
+                { urls: [ '<all_urls>' ] },
+                [ 'blocking', 'responseHeaders' ]);
 
-    let regex = '^';
+            const loadChangedSites = ({ sites }) => {
+                browser.storage.onChanged.removeListener(loadChangedSites);
+                browser.webRequest.onHeadersReceived.removeListener(filterer);
+                const newSites = sites.newValue;
+                loadSites(newSites);
+            };
 
-    if (scheme === '*') {
-        regex += '(http|https)';
-    } else {
-        regex += scheme;
-    }
+            browser.storage.onChanged.addListener(loadChangedSites);
+        });
 
-    regex += '://';
-
-    if (host && host === '*') {
-        regex += '[^/]+?';
-    } else if (host) {
-        if (host.match(/^\*\./)) {
-            regex += '[^/]*?';
-            host = host.substring(2);
-        }
-        regex += host.replace(/\./g, '\\.');
-    }
-
-    if (path) {
-        if (path === '*') {
-            regex += '(/.*)?';
-        } else if (path.charAt(0) !== '/') {
-            regex += '/';
-            regex += path.replace(/\./g, '\\.').replace(/\*/g, '.*?');
-            regex += '/?';
-        }
-    } else {
-        regex += '/?';
-    }
-
-    regex += '$';
-    return new RegExp(regex);
-}
-
-browser.storage.local.get('sites').then(({sites}) => {
-  if (typeof sites === 'object' && Array.isArray(sites)) {
-    regexes = sites.map(matchPatternToRegExp);
-  }
-});
-
-browser.webRequest.onHeadersReceived.addListener(
-  function(details) {
-    const {documentUrl, type, responseHeaders} = details;
-    if (type == 'sub_frame' && documentUrl) {
-      for (const regex of regexes) {
-        if (documentUrl.match(regex)) {
-          return {
-            responseHeaders: responseHeaders.filter(({name}) => name.toLowerCase() != "x-frame-options"),
-          };
-        }
-      }
-    }
-    return {};
-  },
-  { urls: ["<all_urls>"] },
-  ["blocking", "responseHeaders"]
-);
-
-browser.storage.onChanged.addListener(function (items, areaName) {
-  const {sites} = items;
-  const newSites = sites.newValue;
-  if (typeof newSites === 'object' && Array.isArray(newSites)) {
-    regexes = newSites.map(matchPatternToRegExp);
-  }
-});
+loadSites();
